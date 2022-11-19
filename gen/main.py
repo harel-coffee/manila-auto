e262a4a15ac72f4c84dd314174a87bd860464357
GIordano <giordano.daloisio@gmail.com>
2022-11-19 19:54:33 +0100

    added other metrics

--- gen/main.py
+++ gen/main.py
@@ -4,7 +4,6 @@ import pickle
 from copy import deepcopy
 from utils import *
 from demv import DEMV
-from fairlearn.reductions import ExponentiatedGradient, BoundedGroupLoss, ZeroOneLoss, GridSearch, DemographicParity
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -53,20 +52,6 @@ def exec(data):
             'reweighing',
             'dir',
         ],
- 
-        'inprocessing': [
-            'eg',
-            'grid',
-            'adv',
-   
-            'gerry',
-            'meta',
-            'prej',
-        ],
-        'postprocessing': [
-            'cal',
-            'rej',
-        ]
     }
 
     base_metrics = {
@@ -81,10 +66,14 @@ def exec(data):
         'tpr_diff': [],
         'fpr_diff': [],
         'acc': [],
-        'hmean': [],
+        'mean': [],
     }
 
-    agg_metric =  'hmean' 
+
+
+
+    agg_metric = 'mean' 
+
     dataset_label =  'binary' 
     ris = pd.DataFrame()
     for m in ml_methods.keys():
@@ -135,25 +124,7 @@ def exec(data):
     #     data = demv.fit_transform(data, sensitive_features, label)
     #     model.fit(data.drop(label,axis=1).values, data[label].values.ravel())
     #     return model, report
-    #     #     # if best_ris['fairness_method'] == 'eg':
-    #     if dataset_label == 'binary':
-    #         constr = BoundedGroupLoss(DemographicParity(), upper_bound=0.1)
-    #     else:
-    #         constr = BoundedGroupLoss(ZeroOneLoss(), upper_bound=0.1)
-    #     eg = ExponentiatedGradient(
-    #         model, constr, sample_weight_name="classifier__sample_weight")
-    #     eg.fit(data.drop(label, axis=1).values, data[label].values.ravel(),sensitive_features=data[sensitive_features])
-    #     return eg, report
-    #     #     # else:
-    #     if dataset_label == 'binary':
-    #         constr = BoundedGroupLoss(DemographicParity(), upper_bound=0.1)
-    #     else:
-    #         constr = BoundedGroupLoss(ZeroOneLoss(), upper_bound=0.1)
-    #     grid = GridSearch(
-    #         model, constr, sample_weight_name="classifier__sample_weight")
-    #     grid.fit(data.drop(label, axis=1).values, data[label].values.ravel(),sensitive_features=data[sensitive_features])
-    #     return grid, report
-    #     # 
+    #     #     #     # 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Experiment file for fairness testing')
--- gen/utils.py
+++ gen/utils.py
@@ -1,22 +1,15 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold
-from fairlearn.reductions import BoundedGroupLoss, GridSearch, ExponentiatedGradient, DemographicParity, ZeroOneLoss
 from aif360.datasets import BinaryLabelDataset
 from aif360.sklearn.preprocessing import Reweighing
 from aif360.algorithms.preprocessing import DisparateImpactRemover
 from copy import deepcopy
 from scipy import stats
 from demv import DEMV
-from aif360.sklearn.inprocessing import AdversarialDebiasing
-import tensorflow.compat.v1 as tf
-from aif360.algorithms.inprocessing import GerryFairClassifier
-from aif360.algorithms.inprocessing import MetaFairClassifier
-from aif360.algorithms.inprocessing import PrejudiceRemover
-from aif360.sklearn.postprocessing import PostProcessingMeta
-from aif360.sklearn.postprocessing import CalibratedEqualizedOdds
-from aif360.sklearn.postprocessing import RejectOptionClassifierCV
 from metrics import *
+import statistics
+
 # TRAINING FUNCTIONS
 
 def cross_val(classifier, data, label, unpriv_group, priv_group, sensitive_features, positive_label, metrics, n_splits=10, preprocessor=None, inprocessor=None, postprocessor=None):
@@ -46,51 +39,15 @@ def cross_val(classifier, data, label, unpriv_group, priv_group, sensitive_featu
         if preprocessor == 'demv':
             demv = DEMV(round_level=1)
             df_train = demv.fit_transform(df_train, [keys for keys in unpriv_group.keys()], label)
-        if inprocessor == 'eg':
-            constr = _get_constr(df_train, label)
-            model = ExponentiatedGradient(
-                model, constraints=constr, sample_weight_name="classifier__sample_weight")
-        if inprocessor == 'grid':
-            constr = _get_constr(df_train, label)
-            model = GridSearch(
-            model, constr, sample_weight_name="classifier__sample_weight")
-        if inprocessor == 'adv':
-            df_train = df_train.set_index([s for s in sensitive_features])
-            df_test = df_test.set_index([s for s in sensitive_features])
-            model = AdversarialDebiasing(prot_attr=[s for s in sensitive_features])
-            tf.disable_eager_execution()
-        if inprocessor == 'gerry':
-            model = GerryFairClassifier(fairness_def='FP')
-        if inprocessor == 'meta':
-            model = MetaFairClassifier(sensitive_attr=sensitive_features[0])
-        if inprocessor == 'prej':
-            model = PrejudiceRemover(sensitive_attr=sensitive_features[0], class_attr=label)
-        if inprocessor == 'gerry' or inprocessor == 'meta' or inprocessor == 'prej':
-            pred, model = _train_gerry_meta(df_train, df_test, label, model, sensitive_features, positive_label)
-        else:
-            exp = bool(inprocessor == 'eg' or inprocessor == 'grid')
-            adv = bool(inprocessor == 'adv')
-            pred, model = _model_train(df_train, df_test, label, model, sensitive_features, exp=exp, weights=weights, adv=adv)
+        exp = bool(inprocessor == 'eg' or inprocessor == 'grid')
+        adv = bool(inprocessor == 'adv')
+        pred, model = _model_train(df_train, df_test, label, model, sensitive_features, exp=exp, weights=weights, adv=adv)
         if postprocessor:
             df_train = df_train.set_index(sensitive_features[0])
             df_test = df_test.set_index(sensitive_features[0])
-        if postprocessor=='cal':
-            cal = CalibratedEqualizedOdds(prot_attr=sensitive_features[0])
-            model, pred = _compute_postprocessing(model, cal, df_train, df_test, label)
-        if postprocessor == 'rej':
-            rej = RejectOptionClassifierCV(
-                scoring='statistical_parity', prot_attr='sex')
-            model, pred = _compute_postprocessing(
-                model, rej, df_train, df_test, label)
         compute_metrics(pred, unpriv_group, label, positive_label, metrics, sensitive_features)
     return model, metrics
 
-def _get_constr(df, label):
-    if len(df[label].unique()) == 2:
-        constr = DemographicParity()
-    else:
-        constr = BoundedGroupLoss(ZeroOneLoss(), upper_bound=0.1)
-    return constr
 
 def _train_test_split(df_train, df_test, label):
     x_train = df_train.drop(label, axis=1)
@@ -118,26 +75,7 @@ def _model_train(df_train, df_test, label, classifier, sensitive_features, exp=F
         model.sess_.close()
     return df_pred, model
 
-def _train_gerry_meta(df_train, df_test, label, model, sensitive_features, positive_label):
-    bin_train = BinaryLabelDataset(favorable_label=positive_label, 
-        unfavorable_label=1-positive_label,
-        df=df_train, 
-        label_names=[label], 
-        protected_attribute_names=sensitive_features)
-    model.fit(bin_train)
-    bin_test = BinaryLabelDataset(favorable_label=positive_label, 
-        unfavorable_label=1-positive_label,
-        df=df_test, 
-        label_names=[label], 
-        protected_attribute_names=sensitive_features)
-    df_pred = _predict_data(model, df_test, label, bin_test, True)
-    return df_pred, model
 
-def _compute_postprocessing(model, postprocessor, d_train, d_test, label):
-    meta = PostProcessingMeta(model, postprocessor)
-    meta.fit(d_train.drop(label, axis=1), d_train[label])
-    df_pred = _predict_data(meta, d_test, label, d_test.drop(label, axis=1))
-    return meta, df_pred
 
 def _predict_data(model, df_test, label, x_test, aif_data=False):
     pred = model.predict(x_test)
@@ -177,22 +115,21 @@ def compute_metrics(df_pred, unpriv_group, label, positive_label, metrics, sensi
     metrics['recall'].append(recall_score)
     auc_score = auc(df_pred, label)
     metrics['auc'].append(auc_score)
-    metrics['hmean'].append(
-        stats.hmean([
+    metrics['mean'].append(
+        statistics.mean([
             accuracy_score,
  
             di,
  
-            norm_data(eo), 
-            norm_data(stat_par), 
-            norm_data(zero_one_loss),
-            norm_data(ao),
+            eo, 
+            stat_par, 
+            zero_one_loss,
+            ao,
             precision_score,
             recall_score,
             auc_score,
-            norm_data(tpr),
-            norm_data(fpr),
+            tpr,
+            fpr,
         ])
     )
     return metrics
-
